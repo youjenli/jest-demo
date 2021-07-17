0d6f387add311ddae662b6394d7ea84a951ff8dd
youjenli <youjenli1124@gmail.com>
2021-07-17 17:23:35 +0800

    為專案加入建置指令

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,10 @@ var doSomethingElse;
 (function () {
     doSomething = () => {
         console.log('do something');
+        return 3;
     };
     doSomethingElse = () => {
         console.log('do something else');
+        return 5;
     };
 })();
